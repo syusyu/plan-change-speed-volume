f6a123ce51b8282f8f34ccf9010a4c57d413288a
syusyu <syuduckdv@hotmail.com>
2016-10-19 18:38:50 +0900

    SPA finally

--- src/js/service_contract_plan_speed_volume.js
+++ src/js/service_contract_plan_speed_volume.js
@@ -9,11 +9,11 @@ var plan_speed_volume = (function () {
         initModule, initLogger, activateModule;
 
     preparePage = function () {
-        plan_speed_volume.preparePage();
+        plan_speed_volume.model.preparePage();
     };
 
     updateSpeed = function () {
-        plan_speed_volume.model.updatePlan();
+        plan_speed_volume.model.updateSpeed();
     }
 
     getLogger = function () {
@@ -30,7 +30,8 @@ var plan_speed_volume = (function () {
         }
         initLogger(is_debug_mode);
 
-        spa_page_transition.addAction('next-to-change-speed', 'plan-speed-change', preparePage);
+        spa_page_transition.addAction('initialize', 'plan-detail-main', preparePage);
+        spa_page_transition.addAction('next-to-change-speed', 'plan-speed-change');
         spa_page_transition.addAction('back-to-plan-detail-main', 'plan-detail-main');
         spa_page_transition.addAction('update-speed', 'plan-speed-complete', updateSpeed);
 
@@ -41,8 +42,11 @@ var plan_speed_volume = (function () {
     };
 
     return {
+        preparePage: preparePage,
+        updateSpeed: updateSpeed,
         getLogger: getLogger,
         initModule: initModule,
+        INIT_DATA_EVENT: INIT_DATA_EVENT,
         //VisibleForTesting
         initLogger: initLogger,
     }
@@ -62,43 +66,88 @@ plan_speed_volume.shell = (function () {
 
 plan_speed_volume.model = (function () {
     var
-        serverData,
+        serverHost, server_data,
+        preparePage, updateSpeed,
         initModule;
 
-    serverData = (function () {
+    server_data = (function () {
         var
-            headData, planList,
-            prepareServerData, getHeadData, getPlanList, findPlan;
+            srv_data,
+            prepare, get_srv_data;
 
-        prepareServerData = function (data) {
-            headData = data.head;
-            planList = data.head.plans;
+        prepare = function (data) {
+            srv_data = data;
         };
-        getHeadData = function () {
-            return headData;
+        get_srv_data = function () {
+            return srv_data;
         };
-        getPlanList = function () {
-            return planList;
-        };
-        findPlan = function (id) {
-            if (!id) {
-                return null;
-            }
-            return planList.filter(function (obj, idx) {
-                    return obj.id === id
-                })[0] || null;
-        };
-
         return {
-            prepareServerData: prepareServerData,
-            getHeadData: getHeadData,
-            getPlanList: getPlanList,
-            findPlan: findPlan,
+            prepare: prepare,
+            get_srv_data: get_srv_data,
         }
     })();
 
+    preparePage = function () {
+        plan_speed_volume.getLogger().debug('model.preparePage is executed.');
+        $(spa_page_transition.DATA_BIND_EVENT).trigger(plan_speed_volume.INIT_DATA_EVENT, server_data.get_srv_data());
+    };
+
+    updateSpeed = function () {
+        plan_speed_volume.getLogger().debug('model.updateSpeed is executed.');
+    }
+
+    initModule = function (server_host, send_params) {
+        var
+            dfd_result = $.Deferred();
+
+        serverHost = server_host;
+        plan_speed_volume.data.doAccessServerWrapper(serverHost + plan_speed_volume.data.PATH_INIT, send_params, dfd_result, function (data) {
+            plan_speed_volume.getLogger().debug('initial data loaded! status', data.status, 'init_data', data.init_data);
+            server_data.prepare(data);
+        });
+        plan_speed_volume.getLogger().debug('service.model.initModule executed.');
+        spa_page_transition.prepareActivation(dfd_result.promise());
+    };
+
     return {
-        serverData: serverData,
+        preparePage: preparePage,
+        updateSpeed: updateSpeed,
         initModule: initModule,
     }
 })();
+
+plan_speed_volume.data = (function () {
+    'use strict';
+    var
+        doAccessServerWrapper, PATH_INIT, PATH_UPDATE;
+
+    PATH_INIT = '/server_response_initialize.json';
+    PATH_UPDATE = '/server_response_update.json';
+    // PATH_INIT = '/changeplan/init';
+    // PATH_UPDATE = '/changeplan/update';
+
+    doAccessServerWrapper = function (filePath, send_params, dfd_result, succeeded_func, failed_func) {
+        setTimeout(function () {
+            spa_page_data.doAccessServer(filePath, send_params, function (data) {
+                if (data.status === '0') {
+                    if (succeeded_func) {
+                        succeeded_func(data);
+                    }
+                    dfd_result.resolve(data);
+                } else {
+                    if (failed_func) {
+                        failed_func(data);
+                    }
+                    dfd_result.reject(data.message);
+                }
+            });
+        }, 1000);
+    };
+
+    return {
+        doAccessServerWrapper: doAccessServerWrapper,
+        PATH_INIT: PATH_INIT,
+        PATH_UPDATE: PATH_UPDATE,
+    }
+
+}());
