541d0dca9213a04c671f2d5a4227bde32ade1b75
syusyu <syuduckdv@hotmail.com>
2016-12-20 16:55:51 +0900

    First commit for volume

--- src/js/service_contract_plan_speed_volume.js
+++ src/js/service_contract_plan_speed_volume.js
@@ -13,9 +13,10 @@ var plan_speed_volume = (function () {
             PATH_INIT = server_host + '/data_init_low_speed.json',
             // PATH_INIT = server_host + '/data_init_high_speed.json',
             // PATH_INIT = server_host + '/data_init_disable.json',
-            PATH_UPDATE = server_host + '/data_update_succeeded_high.json',
-            // PATH_UPDATE = server_host + '/data_update_succeeded_low.json',
-            // PATH_UPDATE = server_host + '/data_update_failed.json',
+            PATH_UPDATE_SPEED = server_host + '/data_update_succeeded_high.json',
+            // PATH_UPDATE_SPEED = server_host + '/data_update_succeeded_low.json',
+            // PATH_UPDATE_SPEED = server_host + '/data_update_failed.json',
+            PATH_UPDATE_VOLUME = server_host + '/data_update_succeeded_high.json',
 
             callbackFuncProto = {
                 callbackFunc: function (observer, anchor_map, data) {
@@ -26,6 +27,7 @@ var plan_speed_volume = (function () {
                     logger.debug('Ajax proc done! data', data);
                     server_data.prepare(data);
                     observer.trigger('SPEED', server_data.get_speed_status());
+                    observer.trigger('VOLUME', server_data.get_volume_status());
                 }
             },
 
@@ -38,7 +40,7 @@ var plan_speed_volume = (function () {
                 return result;
             },
 
-            createUpdateCallBack = function () {
+            createUpdateSpeedCallBack = function () {
                 var result = Object.create(callbackFuncProto);
                 result.callbackFunc = function (observer, anchor_map, data) {
                     callbackFuncProto.callbackFunc.apply(this, arguments);
@@ -47,12 +49,18 @@ var plan_speed_volume = (function () {
                 return result;
             },
 
-            initializationFunc = spa_page_transition.createAjaxFunc(
-                PATH_INIT, send_params, createInitCallBack().callbackFunc),
+            createUpdateVolumeCallBack = function () {
+                var result = Object.create(callbackFuncProto);
+                result.callbackFunc = function (observer, anchor_map, data) {
+                    callbackFuncProto.callbackFunc.apply(this, arguments);
+                    observer.trigger('SPEED_UPDATE', server_data.get_speed_update());
+                };
+                return result;
+            },
 
-            updateSpeed = spa_page_transition.createAjaxFunc(
-                PATH_UPDATE, {'contractPlanId': contract_plan_id}, createUpdateCallBack().callbackFunc),
-
+            initializationFunc = spa_page_transition.createAjaxFunc(PATH_INIT, send_params, createInitCallBack().callbackFunc),
+            updateSpeed = spa_page_transition.createAjaxFunc(PATH_UPDATE_SPEED, {'contractPlanId': contract_plan_id}, createUpdateSpeedCallBack().callbackFunc),
+            updateVolume = spa_page_transition.createAjaxFunc(PATH_UPDATE_VOLUME, {}, createUpdateVolumeCallBack().callbackFunc),
             logger = spa_log.createLogger(is_debug_mode, '### PLAN_CHANGE.LOG ###');
 
         if (spa_page_util.isEmpty(server_host)) {
@@ -61,31 +69,46 @@ var plan_speed_volume = (function () {
             spa_page_transition.debugMode(is_debug_mode).initialize(initializationFunc)
                 .addAction(spa_page_transition.model.START_ACTION, 'plan-detail-main')
                 .addAction('next-to-change-speed', 'plan-speed-change')
+                .addAction('next-to-add-volume', 'plan-volume-add')
                 .addAction('back-to-plan-detail-main', 'plan-detail-main')
                 .addAction('update-speed', 'plan-speed-complete', [updateSpeed])
+                .addAction('update-volume', 'plan-volume-complete', [updateVolume])
                 .run();
         }
     };
 
     server_data = (function () {
         var
-            _speed_status, _speed_update,
             _plan_data, get_plan_data,
-            prepare, get_speed_status, get_speed_update
+            _speed_status, _speed_update, get_speed_status, get_speed_update,
+            _volume_status, _volume_update, _volume_pack_list, get_volume_status, get_volume_update, get_volume_pack_list,
+            prepare;
 
         prepare = function (data) {
             _speed_status = data.speed_status;
             _speed_update = data.speed_update;
+            _volume_status = data.volume_status;
+            _volume_update = data.volume_update;
+            // _volume_pack_list = data.volume_pack_list;
             _plan_data = data.plan_data;
         };
 
         get_speed_status = function () {
             return _speed_status;
         };
-
         get_speed_update = function () {
             return _speed_update;
         };
+
+        get_volume_status = function () {
+            return _volume_status;
+        }
+        get_volume_update = function () {
+            return _volume_update;
+        }
+        get_volume_pack_list = function () {
+            return _volume_pack_list;
+        }
 
         get_plan_data = function () {
             return _plan_data;
@@ -95,6 +118,9 @@ var plan_speed_volume = (function () {
             prepare: prepare,
             get_speed_status: get_speed_status,
             get_speed_update: get_speed_update,
+            get_volume_status: get_volume_status,
+            get_volume_update: get_volume_update,
+            get_volume_pack_list: get_volume_pack_list,
             get_plan_data: get_plan_data,
         }
     })();
