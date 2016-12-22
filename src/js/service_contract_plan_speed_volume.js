078beb21fd097c3b7c9aad2b11ad8c05d4ebdfa6
syusyu02 <syuduckdv@hotmail.com>
2016-12-22 17:22:02 +0900

    Thinking about validateVolumeAdd

--- src/js/service_contract_plan_speed_volume.js
+++ src/js/service_contract_plan_speed_volume.js
@@ -49,6 +49,24 @@ var plan_speed_volume = (function () {
                 return result;
             },
 
+            selectVolumePack = spa_page_transition.createFunc(function (observer, anchor_map) {
+                if (!anchor_map.val || anchor_map.val < 0) {
+                    $('#volume-add-agree').prop('checked', false).change();
+                }
+                server_data.settle_selected_volume_pack(anchor_map.val);
+                observer.trigger('SELECTED_VOLUME_PACK', server_data.get_selected_volume_pack());
+            }),
+
+            validateVolumePack = spa_page_transition.createFunc(function (observer, anchor_map) {
+                var
+                    selected_volume_pack = server_data.get_selected_volume_pack();
+
+                if (selected_volume_pack.id < 0) {
+                    console.log('volume pack should be selected');
+                    observer.stay();
+                }
+            }),
+
             createUpdateVolumeCallBack = function () {
                 var result = Object.create(callbackFuncProto);
                 result.callbackFunc = function (observer, anchor_map, data) {
@@ -58,21 +76,30 @@ var plan_speed_volume = (function () {
                 return result;
             },
 
+            getParamsForUpdateVolume = function () {
+                return {'id': server_data.get_selected_volume_pack()};
+            },
+
             initializationFunc = spa_page_transition.createAjaxFunc(PATH_INIT, send_params, createInitCallBack().callbackFunc),
+
             updateSpeed = spa_page_transition.createAjaxFunc(PATH_UPDATE_SPEED, {'contractPlanId': contract_plan_id}, createUpdateSpeedCallBack().callbackFunc),
-            updateVolume = spa_page_transition.createAjaxFunc(PATH_UPDATE_VOLUME, {}, createUpdateVolumeCallBack().callbackFunc),
+
+            updateVolume = spa_page_transition.createAjaxFunc(PATH_UPDATE_VOLUME, {}, createUpdateVolumeCallBack().callbackFunc)
+                .get_params(getParamsForUpdateVolume),
+
             logger = spa_log.createLogger(is_debug_mode, '### PLAN_CHANGE.LOG ###');
 
         if (spa_page_util.isEmpty(server_host)) {
             spa_page_transition.renderPage('plan-detail-main');
         } else {
             spa_page_transition.debugMode(is_debug_mode).initialize(initializationFunc)
-                .addAction(spa_page_transition.model.START_ACTION, 'plan-detail-main')
+                .addAction(spa_page_transition.model.START_ACTION, 'plan-detail-main', [selectVolumePack])
                 .addAction('next-to-change-speed', 'plan-speed-change')
                 .addAction('next-to-add-volume', 'plan-volume-add')
                 .addAction('back-to-plan-detail-main', 'plan-detail-main')
+                .addAction('select-volume-pack', 'plan-volume-add', [selectVolumePack])
                 .addAction('update-speed', 'plan-speed-complete', [updateSpeed])
-                .addAction('update-volume', 'plan-volume-complete', [updateVolume])
+                .addAction('update-volume', 'plan-volume-complete', [validateVolumePack])
                 .run();
         }
     };
@@ -80,8 +107,12 @@ var plan_speed_volume = (function () {
     server_data = (function () {
         var
             _plan_data, get_plan_data,
-            _speed_status, _speed_update, get_speed_status, get_speed_update,
-            _volume_status, _volume_update, _volume_pack_list, get_volume_status, get_volume_update, get_volume_pack_list,
+            _speed_status, get_speed_status,
+            _speed_update, get_speed_update,
+            _volume_status, get_volume_status,
+            get_volume_pack_list,
+            _selected_volume_pack, get_selected_volume_pack, settle_selected_volume_pack,
+            _volume_update, get_volume_update,
             prepare;
 
         prepare = function (data) {
@@ -89,13 +120,17 @@ var plan_speed_volume = (function () {
             _speed_update = data.speed_update;
             _volume_status = data.volume_status;
             _volume_update = data.volume_update;
-            // _volume_pack_list = data.volume_pack_list;
             _plan_data = data.plan_data;
+        };
+
+        get_plan_data = function () {
+            return _plan_data;
         };
 
         get_speed_status = function () {
             return _speed_status;
         };
+
         get_speed_update = function () {
             return _speed_update;
         };
@@ -103,11 +138,29 @@ var plan_speed_volume = (function () {
         get_volume_status = function () {
             return _volume_status;
         }
+
         get_volume_update = function () {
             return _volume_update;
         }
+
         get_volume_pack_list = function () {
-            return _volume_pack_list;
+            return _volume_status.volume_pack_list;
+        }
+
+        get_selected_volume_pack = function () {
+            return _selected_volume_pack;
+        }
+
+        settle_selected_volume_pack = function (selected_id) {
+            if (!get_volume_pack_list()) {
+                return;
+            }
+            if (!selected_id) {
+                _selected_volume_pack = get_volume_pack_list()[0];
+            }
+            _selected_volume_pack = get_volume_pack_list().filter(function (el) {
+                return el.id === selected_id
+            })[0] || get_volume_pack_list()[0];
         }
 
         get_plan_data = function () {
@@ -116,12 +169,14 @@ var plan_speed_volume = (function () {
 
         return {
             prepare: prepare,
+            get_plan_data: get_plan_data,
             get_speed_status: get_speed_status,
             get_speed_update: get_speed_update,
             get_volume_status: get_volume_status,
             get_volume_update: get_volume_update,
             get_volume_pack_list: get_volume_pack_list,
-            get_plan_data: get_plan_data,
+            settle_selected_volume_pack: settle_selected_volume_pack,
+            get_selected_volume_pack: get_selected_volume_pack,
         }
     })();
 
