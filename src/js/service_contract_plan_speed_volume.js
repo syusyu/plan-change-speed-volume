e0acfd4ae9c693cd626d0650dee6eafe33b74023
syusyu02 <syuduckdv@hotmail.com>
2017-01-09 22:54:58 +0900

    Add shell and change the name app_status to model

--- src/js/service_contract_plan_speed_volume.js
+++ src/js/service_contract_plan_speed_volume.js
@@ -2,8 +2,56 @@ var plan_speed_volume = (function () {
     'use strict';
     var
         logger,
-        contract_plan_id, app_state,
+        contract_plan_id, model, shell,
         initModule;
+
+    shell = (function () {
+        var
+            hide_error_message = function () {
+                $('#volume-pack-error').removeClass('volume-pack-error-show');
+                $('#volume-pack-error').addClass('volume-pack-error-hide');
+            },
+            show_error_message = function () {
+                $('#volume-pack-error').removeClass('volume-pack-error-hide');
+                $('#volume-pack-error').addClass('volume-pack-error-show');
+            },
+            check_off_volume_agreement = function () {
+                $('#volume-add-agree').prop('checked', false);
+            },
+            init_volume_pack_selection = function () {
+                $('#volume-pack-list').val(-1);
+            },
+            tear_down = function () {
+                hide_error_message();
+                check_off_volume_agreement();
+                init_volume_pack_selection();
+            },
+            get_params_for_update = function () {
+                var
+                    params = {'contractPlanId': contract_plan_id};
+
+                $('input[name^=CHECK_KEY_]').each(function (idx, el) {
+                    params[$(el).attr('name')] = $(el).val();
+                });
+                return params;
+            },
+            get_params_for_update_volume = function () {
+                var
+                    params = get_params_for_update();
+
+                params.volumePackSeq = $('#volume-pack-list').val();
+                return params;
+            };
+        return {
+            show_error_message: show_error_message,
+            hide_error_message: hide_error_message,
+            check_off_volume_agreement: check_off_volume_agreement,
+            init_volume_pack_selection: init_volume_pack_selection,
+            tear_down: tear_down,
+            get_params_for_update: get_params_for_update,
+            get_params_for_update_volume: get_params_for_update_volume,
+        }
+    })();
 
     initModule = function ($server_host, send_params, is_debug_mode) {
         var
@@ -11,20 +59,20 @@ var plan_speed_volume = (function () {
             contract_plan_id = send_params['contractPlanId'],
 
             PATH_INIT = server_host + '/data_init_low_speed.json',
+            PATH_UPDATE_SPEED = server_host + '/data_update_succeeded_high.json',
+            PATH_UPDATE_VOLUME = server_host + '/data_update_succeeded_high.json',
             // PATH_INIT = server_host + '/data_init_high_speed.json',
             // PATH_INIT = server_host + '/data_init_disable.json',
-            PATH_UPDATE_SPEED = server_host + '/data_update_succeeded_high.json',
             // PATH_UPDATE_SPEED = server_host + '/data_update_succeeded_low.json',
             // PATH_UPDATE_SPEED = server_host + '/data_update_failed.json',
-            PATH_UPDATE_VOLUME = server_host + '/data_update_succeeded_high.json',
             // PATH_UPDATE_VOLUME = server_host + '/data_update_failed.json',
 
             callbackFuncProto = {
                 callbackFunc: function (observer, anchor_map, data) {
                     logger.debug('Ajax proc done!');
-                    app_state.prepare(data);
-                    observer.trigger('SPEED', app_state.get_speed_status());
-                    observer.trigger('VOLUME', app_state.get_volume_status());
+                    model.prepare(data);
+                    observer.trigger('SPEED', model.get_speed_status());
+                    observer.trigger('VOLUME', model.get_volume_status());
                 }
             },
 
@@ -33,7 +81,7 @@ var plan_speed_volume = (function () {
                 result.callbackFunc = function (observer, anchor_map, data) {
                     logger.debug('createInitCallBack is called. data', data);
                     callbackFuncProto.callbackFunc.apply(this, arguments);
-                    observer.trigger('PLAN', app_state.get_plan_data());
+                    observer.trigger('PLAN', model.get_plan_data());
                 };
                 return result;
             },
@@ -43,7 +91,7 @@ var plan_speed_volume = (function () {
                 result.callbackFunc = function (observer, anchor_map, data) {
                     logger.debug('createUpdateSpeedCallBack is called. data', data);
                     callbackFuncProto.callbackFunc.apply(this, arguments);
-                    observer.trigger('SPEED_UPDATE', app_state.get_speed_update());
+                    observer.trigger('SPEED_UPDATE', model.get_speed_update());
                 };
                 return result;
             },
@@ -53,17 +101,16 @@ var plan_speed_volume = (function () {
                 result.callbackFunc = function (observer, anchor_map, data) {
                     logger.debug('createUpdateVolumeCallBack is called. data', data);
                     callbackFuncProto.callbackFunc.apply(this, arguments);
-                    observer.trigger('VOLUME_UPDATE', app_state.get_volume_update());
+                    observer.trigger('VOLUME_UPDATE', model.get_volume_update());
                 };
                 return result;
             },
 
             selectVolumePack = spa_page_transition.createFunc(function (observer, anchor_map) {
                 logger.debug('selectVolumePack is called. selected_val', anchor_map.val);
-                app_state.settle_selected_volume_pack(anchor_map.val);
-                $('#volume-pack-error').removeClass('volume-pack-error-show')
-                $('#volume-pack-error').addClass('volume-pack-error-hide')
-                observer.trigger('SELECTED_VOLUME_PACK', app_state.get_selected_volume_pack());
+                model.settle_selected_volume_pack(anchor_map.val);
+                shell.hide_error_message();
+                observer.trigger('SELECTED_VOLUME_PACK', model.get_selected_volume_pack());
             }),
 
             validateVolumePack = spa_page_transition.createFunc(function (observer, anchor_map) {
@@ -72,27 +119,23 @@ var plan_speed_volume = (function () {
 
                 logger.debug('validateVolumePack is called. val=' + selected_volume_val);
                 if (!selected_volume_val || selected_volume_val < 0) {
-                    $('#volume-pack-error').removeClass('volume-pack-error-hide')
-                    $('#volume-pack-error').addClass('volume-pack-error-show')
+                    shell.show_error_message();
                     observer.forward('next-to-add-volume');
                 }
             }),
 
-            getParamsForUpdateVolume = function () {
-                var
-                    params = {'contractPlanId': contract_plan_id, 'volumePackSeq': $('#volume-pack-list').val()};
-
-                $('input[name^=CHECK_KEY_]').each(function (idx, el) {
-                    params[$(el).attr('name')] = $(el).val();
-                });
-                return params;
-            },
+            tearDown = spa_page_transition.createFunc(function (observer, anchor_map) {
+                logger.debug('tearDown is called.')
+                shell.tear_down();
+            }),
 
             initializationFunc = spa_page_transition.createAjaxFunc(PATH_INIT, send_params, createInitCallBack().callbackFunc),
 
-            updateSpeed = spa_page_transition.createAjaxFunc(PATH_UPDATE_SPEED, {'contractPlanId': contract_plan_id}, createUpdateSpeedCallBack().callbackFunc),
+            updateSpeed = spa_page_transition.createAjaxFunc(PATH_UPDATE_SPEED, {},
+                createUpdateSpeedCallBack().callbackFunc).get_params(shell.get_params_for_update),
 
-            updateVolume = spa_page_transition.createAjaxFunc(PATH_UPDATE_VOLUME, {}, createUpdateVolumeCallBack().callbackFunc).get_params(getParamsForUpdateVolume),
+            updateVolume = spa_page_transition.createAjaxFunc(PATH_UPDATE_VOLUME, {},
+                createUpdateVolumeCallBack().callbackFunc).get_params(shell.get_params_for_update_volume),
 
             logger = spa_log.createLogger(is_debug_mode, '### SPEED_VOLUME.LOG ###');
 
@@ -103,15 +146,15 @@ var plan_speed_volume = (function () {
                 .addAction(spa_page_transition.model.START_ACTION, 'plan-detail-main')
                 .addAction('next-to-change-speed', 'plan-speed-change')
                 .addAction('next-to-add-volume', 'plan-volume-add')
-                .addAction('back-to-plan-detail-main', 'plan-detail-main')
+                .addAction('back-to-plan-detail-main', 'plan-detail-main', [tearDown])
                 .addAction('select-volume-pack', 'plan-volume-add', [selectVolumePack])
                 .addAction('update-speed', 'plan-speed-complete', [updateSpeed])
-                .addAction('update-volume', 'plan-volume-complete', [validateVolumePack, updateVolume])
+                .addAction('update-volume', 'plan-volume-complete', [validateVolumePack, updateVolume, tearDown])
                 .run();
         }
     };
 
-    app_state = (function () {
+    model = (function () {
         var
             _plan_data, get_plan_data,
             _speed_status, get_speed_status,
